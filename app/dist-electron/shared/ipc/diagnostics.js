1b5a2413839574cf9ac89281ad618bf8b51733bf
Jason Gray <Gray247@gmail.com>
2026-04-04 16:37:52 -0400

    chore: repo hygiene cleanup (remove generated artifacts, enforce .gitignore)

--- app/dist-electron/shared/ipc/diagnostics.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DIAGNOSTICS_CHANNELS = void 0;
-exports.DIAGNOSTICS_CHANNELS = {
-    openHistory: 'diagnostics:open-history',
-};
-//# sourceMappingURL=diagnostics.js.map
